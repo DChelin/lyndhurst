c5745f0629e9c8adf8274ed78c8a9510aaa62717
DChelin <devlynchelin6@gmail.com>
2023-01-27 11:46:06 +0200

    Correct Files updated.
    index.php
    contact.php

---
+++ CORRECT/public_html/contact.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<meta name="robots" content="index, follow">
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width; initial-scale=1"/>
+
+<title>Lyndhurst Holdings &amp; Management Limited</title>
+
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+
+
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+
+
+
+<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" type="text/css" media="all" href="style.css" />
+<link rel="stylesheet" type="text/css" media="all" href="responsive.css" />
+
+
+
+
+<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="parallax.min.js"></script>
+
+
+
+</head>
+
+
+
+
+
+
+<body>
+
+
+
+<div id="logo">
+	<div class="wrapper">
+			<a href="index.php">
+		<img src="images/logo.jpg" />
+	</a>	
+	</div><!---wrapper-->
+</div><!---logo-->
+
+
+
+<?php include_once("menu.html"); ?>
+
+
+
+
+<div class="parallax-window" data-parallax="scroll" data-image-src="images/banner.jpg" >
+<h2>parent company of <b>various subsidiaries</b></h2>
+</div><!---parallax-window-->
+
+
+
+<div class="parallax-responsive" >
+	<h4>parent company of <b> various subsidiaries</b></h4>
+</div><!---parallax-responsive-->
+
+
+
+
+
+<div id="main-content">
+	<div class="wrapper">
+
+		<h1>Welcome to Lyndhurst Holdings &amp; Management Limited</h1>
+		
+Headquartered in Oxford England we are the parent company of various subsidiaries.<br><br>
+<img src="images/lyndhurst-info-new.png"></img><br>
+</div>
+<!---home-images-->
+		
+		
+		
+		
+		
+	</div><!---wrapper-->
+</div><!---main-content-->
+
+
+
+
+
+<div id="footer">
+	<div class="wrapper">
+	<p>Copyright ©2022 in the city of Oxford Lyndhurst Co number 13925857 All rights reserved.</p>
+	</div><!---wrapper-->
+<div class="clear"></div>
+</div><!---footer-->
+
+
+
+
+
+
+</body>
+</html>
